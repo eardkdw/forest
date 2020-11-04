5d47b5592234f0c8e2b7bc6e9c93f0a966864833
Dan Walker <daniel.walker@ncas.ac.uk>
2020-11-04 15:11:57 +0000

    Remove unused imports

--- forest/barc/front_tool.py
+++ forest/barc/front_tool.py
@@ -1,10 +1,10 @@
-from bokeh.core.properties import Instance
-from bokeh.io import output_file, show
-from bokeh.core.properties import value
+#from bokeh.core.properties import Instance
+#from bokeh.io import output_file, show
+#from bokeh.core.properties import value
 from bokeh.models import EditTool, Drag, Tap, CustomJS
-from bokeh.plotting import figure
-from bokeh.util.compiler import TypeScript
-from forest.barc import text_stamp
+#from bokeh.plotting import figure
+#from bokeh.util.compiler import TypeScript
+#from forest.barc import text_stamp
 
 #from bokeh.models.tools import FrontDrawTool
 
