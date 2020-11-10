69efd111c9c97772f9b98263e4654c8d442cdfb9
cemachelen <h.l.burns@leeds.ac.uk>
2020-11-10 11:59:46 +0000

    :beetle: merge error missed group 10 name inconsistency

--- forest/barc/toolbar.py
+++ forest/barc/toolbar.py
@@ -70,7 +70,7 @@ class BARC:
         # Dropdown Menu of stamp categories
         self.stamp_categories=["Group0 - General meteorological symbols", "Group1 - General meteorological symbols", "Group2 - Precipitation fog ice fog or thunderstorm", "Group3 - Duststorm sandstorm drifting or blowing snow",
                                "Group4 - Fog or ice fog at the time of observation", "Group5 - Drizzle", "Group6 - Rain", "Group7 - Solid precipitation not in showers",
-                               "Group8 - Showery precipitation or precipitation with recent thunderstorm", "Group9 - Thunderstorms", "Group10 - Typhoons"]
+                               "Group8 - Showery precipitation or precipitation with recent thunderstorm", "Group9 - Thunderstorms", "Group10 - Hurricanes and Typhoons"]
         self.dropDown = Select(title="Meteorological symbols:", width=350,
 
                                value="Group0 - General meteorological symbols",
@@ -546,7 +546,7 @@ class BARC:
             'poly_edit': 'poly_edit',
             'textbox': 'textbox',
         }
-        
+
         buttons = []
         for each in buttonspec1:
             button = bokeh.models.widgets.Button(
